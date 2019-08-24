da81007772eca20037b1873327e883e3a4aa268a
samuelfsd <53972764+samuelfsd@users.noreply.github.com>
2019-08-24 10:21:33 -0300

    lista01ex07

---
+++ lista01ex07.py
@@ -0,0 +1,7 @@
+salario = float (input ('Informe o seu salário: '))
+despesas = float (input ('Informe suas despesas mensais: '))
+resto = salario - despesas
+anos = 1000000 / (resto * 12)
+print ('Você poupará 1,000,000 em {:.2f}'.format (anos), 'anos.')
+
+                 
