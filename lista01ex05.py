1edcd9dc527b023509587134ef137ebd45061fab
samuelfsd <53972764+samuelfsd@users.noreply.github.com>
2019-08-24 10:19:00 -0300

    lista01ex05

---
+++ lista01ex05.py
@@ -0,0 +1,4 @@
+dolar = float (input ('Informe o valor da cotação do dólar: '))
+real = float (input ('Quantos reias você deseja converter? '))
+conversao = float(real/dolar)
+print ('Você terá {:.2f}'.format (conversao) ,'dólares.')
