579077be3daa0dd5490b0614b9bcb0cfa7318b1e
samuelfsd <53972764+samuelfsd@users.noreply.github.com>
2019-08-27 16:42:04 -0300

    lista02ex01

---
+++ lista02ex01.py
@@ -0,0 +1,7 @@
+sexo = str(input ('digite (F) para Feminino e (M) para Masculino: '))
+if sexo == 'M' or sexo == 'm':
+    print ('Sexo masculino.')
+elif sexo == 'F' or sexo == 'f':
+    print ('Sexo feminino.')
+else:
+    print ('Sexo inválido.')
