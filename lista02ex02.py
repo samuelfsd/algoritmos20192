5045714b2309789da225df5842db80dac881bb04
samuelfsd <53972764+samuelfsd@users.noreply.github.com>
2019-08-27 16:42:30 -0300

    lista02ex02

---
+++ lista02ex02.py
@@ -0,0 +1,5 @@
+letra = str(input (' Digite uma letra vogal ou consoante: '))
+if letra == 'a' or letra == 'e' or letra == 'i'  or letra == 'o' or letra == 'u' or letra == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or letra == 'U':
+    print('É uma vogal.')
+else:
+    print ('É uma consoante.')
