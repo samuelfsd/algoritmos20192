a393fdec7392c039e65d46ee1d3de919ec591bc4
samuelfsd <53972764+samuelfsd@users.noreply.github.com>
2019-08-31 18:09:40 -0300

    lista02ex09

---
+++ lista02ex09.py
@@ -0,0 +1,19 @@
+numero = int(input('Informe um número correspondente a um dia de semana: '))
+if numero == 1:
+    print('Domingo.')
+elif numero == 2:
+    print ('Segunda.')
+elif numero == 3:
+    print ('Terça.')
+elif numero == 4:
+    print ('Quarta.')
+elif numero == 5:
+    print ('Quinta.')
+elif numero == 6:
+    print ('Sexta.')
+elif numero == 7:
+    print ('Sábado.')
+else:
+    print ('Valor inválido.')
+
+
