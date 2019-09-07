c92f96b39aa6974825d857f632a9a21669997a93
samuelfsd <53972764+samuelfsd@users.noreply.github.com>
2019-09-07 10:19:50 -0300

    lista03ex01

---
+++ lista03ex01.py
@@ -0,0 +1,5 @@
+valor = int(input('Informe um valor entre 0 e 10: '))
+while valor > 10:
+    print ('Valor inválido.')
+    valor = int(input('Informe um valor entre 0 e 10: '))
+print ('Valor válido!')
