5d23bb858a0e12688034d1105c7c3c2c8218ba58
samuelfsd <53972764+samuelfsd@users.noreply.github.com>
2019-09-27 16:53:41 -0300

    Update lista03ex14.py

--- lista03ex14.py
+++ lista03ex14.py
@@ -1,4 +1,16 @@
-v = float(input('Digite um valor: '))
-menor = v
-maior = v
-#É necessário solicitar mais números ao usuário
+qnt = int(input('Digite a quantidade de numeros: '))
+num = int(input('Digite o numero: '))
+total = num
+maior = num
+menor = num
+for i in range (1,qnt):
+    num = int(input('Digite o numero: '))
+    total = num + total
+    if num < menor:
+       menor = num
+    if num > maior:
+       maior = num
+print('A soma é:' , total)
+print('O menor numero é:' , menor)
+print('O maior numero é' , maior)
+#wtf n vi que estava sem nada 
