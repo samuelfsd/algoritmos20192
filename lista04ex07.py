46c27d7eb0db9be6381707591cd55265ccad8ede
samuelfsd <53972764+samuelfsd@users.noreply.github.com>
2019-09-29 08:31:38 -0300

    lista04ex07

---
+++ lista04ex07.py
@@ -0,0 +1,8 @@
+ch = input("Caractere: ")
+for linha in range(8):
+    for coluna in range(8):
+        if linha <= coluna:
+            print(ch, end = '')
+        else:
+            print(end = '')
+    print()
