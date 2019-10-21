583434ba306c638d95a56f71cd3d9b7b2632df5d
samuelfsd <53972764+samuelfsd@users.noreply.github.com>
2019-10-20 22:53:50 -0300

    lista05ex02

---
+++ lista05ex02.py
@@ -0,0 +1,8 @@
+a = [0]*10
+b = [0]*10
+for i in range (10):
+    a[i] = float(input('Informe um número real: '))
+    b[i] = a[i]**2
+print ('Lista informada pelo usuário: ',a)
+print ('Lista com elementos ao quadrado: ',b)
+
